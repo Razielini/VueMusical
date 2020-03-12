c14303862c285ec8e1c7c2902bc02d8d1cdd6a29
Razielini <razielco@gmail.com>
2020-03-12 12:05:19 -0700

    Encoding with netlify?

--- nuxt.config.js
+++ nuxt.config.js
@@ -10,6 +10,7 @@ export default {
     title: process.env.npm_package_name || '',
     meta: [
       { charset: 'utf-8' },
+      { 'accept-encoding': 'gzip, deflate, br' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
     ],
